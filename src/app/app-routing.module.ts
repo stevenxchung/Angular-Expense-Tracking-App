919a0c80317063eae40a0f4b906516d001fb2c63
Steven <stevenxchung@gmail.com>
2018-06-19 18:31:55 -0400

    Add routes to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { DisplayComponent } from './display/display.component';
 
-const routes: Routes = [];
+// This is where the URL routes will be
+const routes: Routes = [
+  {
+    path: '',
+    component: FormComponent
+  },
+  {
+    path: 'expenses',
+    component: DisplayComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
