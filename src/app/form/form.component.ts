c2584bd8911b90886147a9b562f38f33061da9ed
Steven <stevenxchung@gmail.com>
2018-06-21 17:55:11 -0400

    Creates new object and inserts it into the beginning of the database array

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,9 +10,11 @@ export class FormComponent implements OnInit {
 
   // Initialize types
   itemCount: number;
-  expenseName: string;
-  expenseAmount: number;
+  name: string = 'Water';
+  amount: number = 69.99;
   formDB = [];
+
+  public newExp: Expense;
 
   constructor(private dataService: DataService) { }
 
@@ -23,11 +25,28 @@ export class FormComponent implements OnInit {
     this.dataService.updateDB(this.formDB);
   }
 
-  // Add expense (just the name for now) to recent expenses
+  // Add expense to recent expenses
   addExpense() {
-    this.formDB.unshift(this.expenseName);
+    // Create a new object
+    this.newExp = new Expense();
+    // Set the name and amount for this new object equal to what is submitted on the form
+    this.newExp.expenseName = this.name;
+    this.newExp.expenseAmount = this.amount
+
+    // Add the newly created object to the formDB array
+    this.formDB.unshift(this.newExp);
+    // console.log(this.formDB);
+
+    // Reset values after submit
+    this.name = '';
+    this.amount = 0;
     this.itemCount = this.formDB.length;
     this.dataService.updateDB(this.formDB);
   }
 
+}
+// Expense blueprint
+export class Expense {
+  expenseName: string;
+  expenseAmount: number;
 }
