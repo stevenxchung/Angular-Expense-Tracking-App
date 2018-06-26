972f5744f4134d3933a6044e41949ab8093db6b9
Steven <stevenxchung@gmail.com>
2018-06-25 20:36:12 -0400

    Insert default seed data for temp testing

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,16 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 export class DataService {
 
-  private dB = new BehaviorSubject<any>([]);
+  private dB = new BehaviorSubject<any>(
+    [
+      {expenseGroup: "Utilities", expenseName: "Water", expenseAmount: 69.99, timeStamp: "1993-09-29"},
+      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 18.75, timeStamp: "1993-09-30"},
+      {expenseGroup: "Utilities", expenseName: "Internet", expenseAmount: 49.99, timeStamp: "1993-10-01"},
+      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 21.50, timeStamp: "1993-10-10"},
+      {expenseGroup: "Utilities", expenseName: "Electric", expenseAmount: 150.35, timeStamp: "1993-10-05"},
+      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 24.52, timeStamp: "1993-10-18"}
+    ]
+  );
   expense = this.dB.asObservable();
 
   constructor() { }
