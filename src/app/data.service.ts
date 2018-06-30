15413b9f3cbf22f00f18a7fdfc75a419c1b99741
Steven <stevenxchung@gmail.com>
2018-06-30 12:10:41 -0400

    Add units to seed data

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,14 +9,14 @@ export class DataService {
 
   private dB = new BehaviorSubject<any>(
     [
-      {expenseGroup: "Food", expenseName: "New Water", expenseAmount: 89.99, timeStamp: "1993-09-29"},
-      {expenseGroup: "Utilities", expenseName: "Water", expenseAmount: 69.99, timeStamp: "1993-09-29"},
-      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 18.75, timeStamp: "1993-09-30"},
-      {expenseGroup: "Utilities", expenseName: "Internet", expenseAmount: 49.99, timeStamp: "1993-10-01"},
-      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 21.50, timeStamp: "1993-10-10"},
-      {expenseGroup: "Utilities", expenseName: "Electric", expenseAmount: 150.35, timeStamp: "1993-10-05"},
-      {expenseGroup: "Gas", expenseName: "Gas", expenseAmount: 24.52, timeStamp: "1993-10-18"},
-      {expenseGroup: "Other", expenseName: "Concert", expenseAmount: 175.42, timeStamp: "1993-11-10"}
+      {expenseGroup: "Food [$]", expenseName: "New Water", expenseAmount: 89.99, timeStamp: "1993-09-29"},
+      {expenseGroup: "Utilities [$]", expenseName: "Water", expenseAmount: 69.99, timeStamp: "1993-09-29"},
+      {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 18.75, timeStamp: "1993-09-30"},
+      {expenseGroup: "Utilities [$]", expenseName: "Internet", expenseAmount: 49.99, timeStamp: "1993-10-01"},
+      {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 21.50, timeStamp: "1993-10-10"},
+      {expenseGroup: "Utilities [$]", expenseName: "Electric", expenseAmount: 150.35, timeStamp: "1993-10-05"},
+      {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 24.52, timeStamp: "1993-10-18"},
+      {expenseGroup: "Other [$]", expenseName: "Concert", expenseAmount: 175.42, timeStamp: "1993-11-10"}
     ]
   );
   expense = this.dB.asObservable();
