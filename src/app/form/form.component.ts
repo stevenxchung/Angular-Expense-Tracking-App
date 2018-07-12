9b7bd2a5739308595427646ad3cc1ea7203ee360
Steven <stevenxchung@gmail.com>
2018-07-12 14:20:43 -0400

    Remove old logic from form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -47,28 +47,14 @@ import {
   ]
 })
 export class FormComponent implements OnInit {
+  // Use expenseList to store objects in an array
   expenseList: Expense[];
-  // // Initialize types
-  // itemCount: number;
-  // group: string;
-  // name: string = 'Water';
-  // amount: number = 69.99;
-  // date: string = '1993-09-29';
-  // formDB = [];
-
-  // public newExp: Expense;
 
   constructor(private dataService: DataService, private toastr: ToastrService) { }
 
   // Called after data-bound properties of a directive are initialized
   ngOnInit() {
-    // this.dataService.expense.subscribe(res => this.formDB = res);
-    // // Sort database by date from largest to smallest
-    // this.formDB.sort((a, b) => +new Date(b.timeStamp) - +new Date(a.timeStamp));
-    // this.dataService.updateDB(this.formDB);
-    // // Update item count
-    // this.itemCount = this.formDB.length;
-
+    // On app load, will grab data from the firebase database "expenses" and load them onto the Expense[] array
     let x = this.dataService.getData();
     x.snapshotChanges().subscribe(item => {
       this.expenseList = [];
@@ -76,6 +62,7 @@ export class FormComponent implements OnInit {
         let y = element.payload.toJSON();
         y['$key'] = element.key;
         this.expenseList.push(y as Expense);
+        this.expenseList.sort((a, b) => +new Date(b.timeStamp) - +new Date(a.timeStamp));
       });
     });
     this.resetForm();
@@ -83,28 +70,6 @@ export class FormComponent implements OnInit {
 
   // Add expense to recent expenses
   onSubmit(expenseForm: NgForm) {
-    // // Create a new object
-    // this.newExp = new Expense();
-    // // Set the group, name, and amount for this new object equal to what is submitted on the form
-    // this.newExp.expenseGroup = this.group;
-    // this.newExp.expenseName = this.name;
-    // this.newExp.expenseAmount = this.amount;
-    // // Date of actual purchase
-    // this.newExp.timeStamp = this.date;
-
-
-    // // Add the newly created object to the formDB array
-    // this.formDB.unshift(this.newExp);
-    // console.log(this.formDB);
-
-    // // Reset values after submit
-    // this.name = '';
-    // this.amount = 0;
-    // this.itemCount = this.formDB.length;
-
-    // this.formDB.sort((a, b) => +new Date(b.timeStamp) - +new Date(a.timeStamp));
-    // this.dataService.updateDB(this.formDB);
-
     if (expenseForm.value.$key == null) {
       this.dataService.insertExpense(expenseForm.value);
     } else {
@@ -115,6 +80,7 @@ export class FormComponent implements OnInit {
     console.log("Success!");
   }
 
+  // Reset form to empty or null values
   resetForm(expenseForm?: NgForm) {
     if (expenseForm !== null) {
       expenseForm.reset();
@@ -135,14 +101,6 @@ export class FormComponent implements OnInit {
   }
 
   // Remove and item from the list
-  // removeItem(i) {
-  //   let result = confirm("Are you sure you want to delete this entry?");
-  //   // If result is true, the specified entry will be deleted
-  //   if (result) {
-  //     this.formDB.splice(i, 1);
-  //   }
-  //   this.dataService.updateDB(this.formDB);
-  // }
   onDelete(key: string) {
     if (confirm("Are you sure you want to delete this entry?") == true) {
       this.dataService.deleteExpense(key);
@@ -151,11 +109,3 @@ export class FormComponent implements OnInit {
   }
 
 }
-
-// // Expense blueprint
-// export class Expense {
-//   expenseGroup: string;
-//   expenseName: string;
-//   expenseAmount: number;
-//   timeStamp: string;
-// }
