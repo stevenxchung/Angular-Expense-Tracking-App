75ec63911678b356c05940bf7fd2138619fa3422
Steven <stevenxchung@gmail.com>
2018-07-14 11:50:51 -0400

    Add firebase to environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDAJuLO8yY_2j5BecFHOV2IdPSxfP9-i2g",
+    authDomain: "expense-tracking-app-demo.firebaseapp.com",
+    databaseURL: "https://expense-tracking-app-demo.firebaseio.com",
+    projectId: "expense-tracking-app-demo",
+    storageBucket: "expense-tracking-app-demo.appspot.com",
+    messagingSenderId: "574938992428"
+  }
 };
