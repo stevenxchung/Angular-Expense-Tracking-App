15025f7580de444e56bd09df2e1d43e633a83e3d
Steven <stevenxchung@gmail.com>
2018-07-14 10:44:02 -0400

    Remove animations from forms component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -17,34 +17,7 @@ import {
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss'],
-  animations: [
-    trigger('formDB', [
-        transition('* => *', [
-            query(':enter', style({ opacity: 0}), {optional: true}),
-
-            // Animation when adding an element to the list
-            query(':enter', stagger('300ms', [
-              animate('.6s ease-in', keyframes([
-                style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-                style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-                style({opacity: 1, transform: 'translateY(0)', offset: 1}),
-
-              ]))
-            ]), {optional: true}),
-
-            // Animation when removing an element from the list
-            query(':leave', stagger('300ms', [
-              animate('.6s ease-in', keyframes([
-                style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-                style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-                style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
-
-              ]))
-            ]), {optional: true})
-        ])
-    ])
-  ]
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
   // Use expenseList to store objects in an array
