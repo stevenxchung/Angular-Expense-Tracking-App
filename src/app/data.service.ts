e8af18379d754baf4225e86440e024d0ec5d9475
Steven <stevenxchung@gmail.com>
2018-07-14 10:43:13 -0400

    Remove old code from data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,20 +12,6 @@ import { Expense } from './expense.model';
 
 export class DataService {
 
-  // private dB = new BehaviorSubject<any>(
-  //   [
-  //     {expenseGroup: "Food [$]", expenseName: "New Water", expenseAmount: 89.99, timeStamp: "1993-09-29"},
-  //     {expenseGroup: "Utilities [$]", expenseName: "Water", expenseAmount: 69.99, timeStamp: "1993-09-29"},
-  //     {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 18.75, timeStamp: "1993-09-30"},
-  //     {expenseGroup: "Utilities [$]", expenseName: "Internet", expenseAmount: 49.99, timeStamp: "1993-10-01"},
-  //     {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 21.50, timeStamp: "1993-10-10"},
-  //     {expenseGroup: "Utilities [$]", expenseName: "Electric", expenseAmount: 150.35, timeStamp: "1993-10-05"},
-  //     {expenseGroup: "Gas [$]", expenseName: "Gas", expenseAmount: 24.52, timeStamp: "1993-10-18"},
-  //     {expenseGroup: "Other [$]", expenseName: "Concert", expenseAmount: 175.42, timeStamp: "1993-11-10"}
-  //   ]
-  // );
-  // expense = this.dB.asObservable();
-
   expenseList: AngularFireList<any>;
   selectedExpense: Expense = new Expense();
   constructor(private firebase: AngularFireDatabase) { }
@@ -61,9 +47,5 @@ export class DataService {
   deleteExpense($key: string) {
     this.expenseList.remove($key);
   }
-
-  // updateDB(data) {
-  //   this.dB.next(data)
-  // }
 
 }
