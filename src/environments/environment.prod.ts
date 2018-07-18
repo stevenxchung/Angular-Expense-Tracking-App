1a2dc36dc6b4a91c2e582b8d9ed5c159aaab34cb
Steven <stevenxchung@gmail.com>
2018-07-17 20:07:50 -0400

    Remove API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDAJuLO8yY_2j5BecFHOV2IdPSxfP9-i2g",
+    apiKey: "REMOVED",
     authDomain: "expense-tracking-app-demo.firebaseapp.com",
     databaseURL: "https://expense-tracking-app-demo.firebaseio.com",
     projectId: "expense-tracking-app-demo",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDAJuLO8yY_2j5BecFHOV2IdPSxfP9-i2g",
+    apiKey: "**REMOVED**",
     authDomain: "expense-tracking-app-demo.firebaseapp.com",
     databaseURL: "https://expense-tracking-app-demo.firebaseio.com",
     projectId: "expense-tracking-app-demo",
